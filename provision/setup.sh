f4fb539e2d11ced196abb41751a95e98988e3e80
Michael Davis <mike.philip.davis@gmail.com>
2014-02-06 15:43:14 -0600

    Adding ssh config to provision files.  Requires a private key that is ignored and has to be distributed manually.

--- provision/setup.sh
+++ provision/setup.sh
@@ -11,6 +11,13 @@ sudo pip install gunicorn
 echo "Setting up nginx configuration"
 sudo cp /vagrant/provision/files/vinz_nginx /etc/nginx/sites-enabled/default
 
+echo "Moving SSH key file"
+cp /vagrant/provision/files/id_rsa /home/vagrant/.ssh/id_rsa
+cp /vagrant/provision/files/known_hosts /home/vagrant/.ssh/known_hosts
+cp /vagrant/provision/files/config /home/vagrant/.ssh/config
+
+chown -R vagrant:vagrant /home/vagrant/.ssh
+
 echo "Restarting nginx"
 sudo service nginx restart
 
