14b1c054e57b96bb059d5833dddf831ca2b74689
Michael Davis <mike.philip.davis@gmail.com>
2014-02-16 16:53:38 -0600

    altering setup script for folder change

--- provision/setup.sh
+++ provision/setup.sh
@@ -5,7 +5,7 @@ sudo apt-get update
 sudo apt-get install nginx build-essential psmisc python-dev python-pip mongodb -y
 
 echo "Installing python requirements with pip"
-sudo pip install -r /vagrant/app/requirements.txt
+sudo pip install -r /vagrant/server/requirements.txt
 sudo pip install gunicorn
 
 echo "Setting up nginx configuration"
