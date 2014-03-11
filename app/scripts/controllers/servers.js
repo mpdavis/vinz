61f8fc237461bbc8ac8c04e60ae6906d507c6de9
Jacob Hummel <j4cobhummel@gmail.com>
2014-03-11 15:19:11 -0500

    Create server

--- app/scripts/controllers/servers.js
+++ app/scripts/controllers/servers.js
@@ -4,4 +4,9 @@ angular.module('vinzApp')
   .controller('ServersCtrl', ['$scope', 'servers', function ($scope, servers) {
     
     $scope.myServers = servers.getServers();
+
+    $scope.createServer = function(newServer) {
+    	servers.createServer(newServer);
+    	$scope.myServers = servers.getServers();
+    }
   }]);
--- app/scripts/services/servers.js
+++ app/scripts/services/servers.js
@@ -4,7 +4,8 @@ angular.module('vinzApp')
   .factory('servers', function ($http, $resource) {
     // Service logic
     // ...
-    var serversAPI = "/api/servers/:server_id";
+    var serversURL = "/api/servers/"
+    var serversAPI = serversURL + ":server_id";
     var Server = $resource(serversAPI, {server_id:'@id'});
 
     //Servers API
@@ -12,6 +13,9 @@ angular.module('vinzApp')
       getServers: function() {
         var servers = Server.query();
         return servers; 
+      },
+      createServer: function(newServer) {
+        $http.post(serversURL, newServer);
       }
     };
   });
