6057e752c84a7c242097de3246d5c7bf4b6fea73
Michael Davis <mike.philip.davis@gmail.com>
2014-03-27 18:37:48 -0500

    ServerScanner

--- server/scanner/scanner.py
+++ server/scanner/scanner.py
@@ -1,4 +1,6 @@
 
+import pprint
+
 from internal import server as internal_server
 from internal import user as internal_user
 
@@ -6,7 +8,86 @@ from api import ssh_key as api_ssh_key
 from api import user as api_user
 
 
+class ServerScanner():
+
+    server = None
+
+    server_users = None
+    vinz_users = None
+
+    def __init__(self, server, add_users=False, remove_users=False):
+        self.server = server
+        self.add_users = add_users
+        self.remove_users = remove_users
+
+    def get_users_from_server(self):
+        """
+        Gets a list of the users that are currently on the server
+
+        :param server: The server entity to get a list of users from
+        """
+        return set(api_user.get_users_on_host(self.server.hostname))
+
+    def get_users_from_vinz(self):
+        """
+        Gets a list of usernames that should be on the box according to vinz
+
+        :param server: The server to query
+        """
+        return set(self.server.get_usernames())
+
+    def add_user(self, user):
+        """
+        Adds a user to a server.
+
+        :param user: The username of the user to add
+        """
+        if self.add_users:
+            api_user.add_user(user, self.server)
+
+    def remove_user(self, user):
+        """
+        Removes a user from a server.
+
+        :param user: The username of the user to remove
+        """
+        if self.remove_users:
+            api_user.remove_user(user, self.server)
+
+    def get_authorized_key_files(self):
+        """
+        Gets all of the authorized_key files for users on the server.
+        """
+        if not self.server_users:
+            self.server_users = self.get_users_from_server()
+
+        self.authorized_keys = api_ssh_key.get_authorized_keys_for_host(self.server.hostname,
+                                                                        self.server_users)
+
+    def scan(self):
+        if not self.server:
+            raise ValueError('There must be a server to scan.')
+
+        server_users = self.get_users_from_server()
+        vinz_users = self.get_users_from_vinz()
+
+        add_users = vinz_users.difference(server_users)
+        if self.add_users and self.add_users:
+            for user in add_users:
+                self.add_user(user)
+
+        remove_users = server_users.difference(vinz_users)
+        if self.remove_users and self.remove_users:
+            for user in remove_users:
+                self.remove_user(user)
+
+        self.get_authorized_key_files()
+        return self.authorized_keys
+
+
 class Scanner():
+
+    scan_state = dict()
 
     def __init__(self, servers=None, add_users=False, remove_users=False):
         if not servers:
@@ -14,7 +95,6 @@ class Scanner():
         self.servers = servers
         self.add_users = add_users
         self.remove_users = remove_users
-        self.scan_state = dict()
 
     def get_servers(self):
         """
@@ -22,73 +102,15 @@ class Scanner():
         """
         return internal_server.get_servers()
 
-    def get_users_from_server(self, server):
-        """
-        Gets a list of the users that are currently on the server
-
-        :param server: There server to get a list of users from
-        """
-        return set(api_user.get_users_on_host(server.hostname))
-
-    def get_users_from_vinz(self, server):
-        """
-        Gets a list of usernames that should be on the box according to vinz
-
-        :param server: The server to query
-        """
-        return set(server.get_usernames())
-
-    def add_user(self, user, server):
-        """
-        Adds a user to a server.
-
-        :param user: The username of the user to add
-        :param server: The server entity to add the user to
-        """
-        if self.add_users:
-            api_user.add_user(user, server)
-
-    def remove_user(self, user, server):
-        """
-        Removes a user from a server.
-
-        :param user: The username of the user to remove
-        :param server: The server entity to remove the user from
-        """
-        if self.remove_users:
-            api_user.remove_user(user, server)
-
-    def get_authorized_key_files(self, server):
-        """
-        Gets all of the authorized_key files for users on the server.
-        """
-        users = self.get_users_from_server(server)
-        return api_ssh_key.get_authorized_keys_for_host(server.hostname, users)
-
-    def scan_server(self, server):
-        """
-        Scans a single server for appropriate users and ssh keys.
-
-        :param server: The server to scan
-        """
-        server_users = self.get_users_from_server(server)
-        vinz_users = self.get_users_from_vinz(server)
-
-        add_users = vinz_users.difference(server_users)
-        if add_users and self.add_users:
-            for user in add_users:
-                self.add_user(user, server)
-
-        remove_users = server_users.difference(vinz_users)
-        if remove_users and self.remove_users:
-            for user in remove_users:
-                self.remove_user(user, server)
-
-        return self.get_authorized_key_files(server)
-
     def scan(self):
         """
         Kicks off the entire Scanner
         """
         for server in self.servers:
-            self.scan_state[server.hostname] = self.scan_server(server)
+            server_scanner = ServerScanner(server,
+                                           add_users=self.add_users,
+                                           remove_users=self.remove_users)
+
+            self.scan_state[server.hostname] = server_scanner.scan()
+        pp = pprint.PrettyPrinter()
+        pp.pprint(self.scan_state)
