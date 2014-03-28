fc74290c91d296c5930e47f8cc90cbf037a6ca45
Zach Heilman <organman91@gmail.com>
2014-03-27 20:48:18 -0500

    adding and removing public keys

--- server/scanner/api/ssh_key.py
+++ server/scanner/api/ssh_key.py
@@ -59,19 +59,25 @@ def check_user_ssh_key(username, inventory, vinz_private_key_path):
     """
 
 
-def set_user_ssh_key(username, inventory, user_public_key, vinz_private_key_path):
+def add_user_public_key(username, hosts, user_public_key):
     """
     :param username: The username of the user on the remote machines
-    :param inventory: A list of servers to add the public key to
-    :param user_public_key: A string represnetation of the user's public key
-    :param vinz_private_key_path: The path to the private key needed to access the remote machines
+    :param hosts: A list of servers to add the public key to
+    :param user_public_key: A string representation of the user's public key
     """
+    module_args = 'state=present user=%s key=\"%s\"' % (username, user_public_key)
+    runner = VinzRunner(hosts=hosts, module_name='authorized_key', module_args=module_args)
+    response = runner.run()
+    return response
 
 
-def remove_user_ssh_key(username, inventory, user_public_key, vinz_private_key_path):
+def remove_user_public_key(username, hosts, user_public_key):
     """
     :param username: The username of the user on the remote machines
-    :param inventory: A list of servers to remove the user's public ssh key from
+    :param hosts: A list of servers to remove the user's public ssh key from
     :param user_public_key: The key to remove
-    :param vinz_private_key_path: The path to the private key needed to access the remote machines
     """
+    module_args = 'state=absent user=%s key=\"%s\"' % (username, user_public_key)
+    runner = VinzRunner(hosts=hosts, module_name='authorized_key', module_args=module_args)
+    response = runner.run()
+    return response
