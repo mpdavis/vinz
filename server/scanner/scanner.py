80299bf11220ad423aa390d3a7c3b63d2c9413d4
Michael Davis <mike.philip.davis@gmail.com>
2014-04-03 18:39:58 -0500

    Parsing key files from the db and the server to decide what to do with them

--- server/scanner/scanner.py
+++ server/scanner/scanner.py
@@ -67,50 +67,80 @@ class ServerScanner():
             for user in remove_users:
                 self.remove_user(user)
 
-    def get_authorized_key_files(self):
+    def get_keys_from_server(self):
         """
         Gets all of the authorized_key files for users on the server.
         """
         if not self.server_users:
             self.server_users = self.get_users_from_server()
 
-        self.authorized_keys = api_ssh_key.get_authorized_keys_for_host(self.server.hostname,
-                                                                        self.server_users)
+        self.keys_from_server = api_ssh_key.get_authorized_keys_for_host(self.server.hostname, self.server_users)
 
-    def parse_authorized_keys(self):
+    def parse_keys(self):
         """
         Parses the authorized_keys files for all of the users on the server
         """
-        for user, keys in self.authorized_keys.iteritems():
-            pass
 
-    def scan_authorized_keys(self):
+        keys_to_remove = dict()
+        keys_to_add = dict()
+
+        for user, keys in self.keys_from_server.iteritems():
+
+            # converting both key lists to sets
+            server_key_set = set(keys)
+            db_key_set = set(self.keys_from_db.get(user, []))
+
+            if not server_key_set.symmetric_difference(db_key_set):
+                # Server and DB agree.  Nothing to do
+                continue
+
+            keys_to_add[user] = db_key_set.difference(server_key_set)
+            keys_to_remove[user] = server_key_set.difference(db_key_set)
+
+        self.keys_to_add = keys_to_add
+        self.keys_to_remove = keys_to_remove
+
+    def scan_keys(self):
         """
         Handles the authorized_key files for this scanner
         """
-        self.get_authorized_key_files()
-        self.parse_authorized_keys()
+
+        self.keys_from_db = {
+            'vinz': ['ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCrFNYehGLTslKj+YBUv4Uo2Gb2QB2IvnkTUY6JbEpl0USrObi8q+kWuV5Yhk+eUszxqu4vIIkFw1B3UK8CH76W5Fu3pcFXhBpui0h/IvDHLePmddFfx/kptdZ0qCs0VxrZgltpjCD8PtWx5nde10xYLI6V/j6yFLao/flB0qt0SJxoIbUdI0Zk99TzOmR4A5yGdW158Nvcsd+bwXshHfmjn3uafksjdSnlcqyNClo1oR3pUJKX9dQuyLA6hlGzF5/f2Sf+eggOkpLcvY7/yStfQMFF6uLZq9DHQAlXsEVnFOmHBGqoFcRKNmV2I7kcJL9QMVxWJrNIkLjBmyxACCsf example@getvinz.com']
+        }
+
+        self.get_keys_from_server()
+        self.parse_keys()
 
     def scan(self):
         if not self.server:
             raise ValueError('There must be a server to scan.')
 
         self.scan_users()
-        self.scan_authorized_keys()
+        self.scan_keys()
 
-        return self.authorized_keys
+        return self.keys_from_db
 
 
 class Scanner():
 
     scan_state = dict()
 
-    def __init__(self, servers=None, add_users=False, remove_users=False):
+    def __init__(
+            self,
+            servers=None,
+            add_users=False,
+            remove_users=False,
+            add_keys=False,
+            remove_keys=False):
+
         if not servers:
             servers = self.get_servers()
         self.servers = servers
         self.add_users = add_users
         self.remove_users = remove_users
+        self.add_keys = add_keys
+        self.remove_keys = remove_keys
 
     def get_servers(self):
         """
