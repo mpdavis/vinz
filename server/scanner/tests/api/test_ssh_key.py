0030503cb186a168483b09e7e481c9b8e902879e
Michael Davis <mike.philip.davis@gmail.com>
2014-04-06 16:06:21 -0500

    Breaking the build

--- server/scanner/tests/api/test_ssh_key.py
+++ server/scanner/tests/api/test_ssh_key.py
@@ -31,7 +31,7 @@ EXAMPLE_CORRECT_RETURN_VALUE = {
 
 CORRECT_OUTPUT = {
     'vinz': [
-        u'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDBffoYSqLvMeUOLcKXkeen7wj/t9rwtaKGJ3Mjq//8Zfl5kBrqV2JqT3BoNGFgjHm1Hrzk1873kOcajK/b/8mQahx77LDMT7Mi4gvoVJ7/U2u6LCI7tLeqSqgI7GJFRdz3RpsCz6iUOp5j5eGvLO4g8YE+P0WByCgv6hajsi+2f6zdPuQdcckHj8GO/Tpf1uIcibiUfAqzkTshhJFfI0oZg/ba2Q5O8iLCO9DBTO9aQg26LiEE53/d9nHlvwAv9ER94ZPjXpNj1vV+HWN2cK+I0ZO2UBjUIDbMXLId5yNEzsKpyhoKso/ThELk95dzQkKzPBicRYCiVWoFLJlnmLOx mike.philip.davis@gmail.com',
+        u'ssh-rsa AAAB3NzaC1yc2EAAAADAQABAAABAQDBffoYSqLvMeUOLcKXkeen7wj/t9rwtaKGJ3Mjq//8Zfl5kBrqV2JqT3BoNGFgjHm1Hrzk1873kOcajK/b/8mQahx77LDMT7Mi4gvoVJ7/U2u6LCI7tLeqSqgI7GJFRdz3RpsCz6iUOp5j5eGvLO4g8YE+P0WByCgv6hajsi+2f6zdPuQdcckHj8GO/Tpf1uIcibiUfAqzkTshhJFfI0oZg/ba2Q5O8iLCO9DBTO9aQg26LiEE53/d9nHlvwAv9ER94ZPjXpNj1vV+HWN2cK+I0ZO2UBjUIDbMXLId5yNEzsKpyhoKso/ThELk95dzQkKzPBicRYCiVWoFLJlnmLOx mike.philip.davis@gmail.com',
         u'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCrFNYehGLTslKj+YBUv4Uo2Gb2QB2IvnkTUY6JbEpl0USrObi8q+kWuV5Yhk+eUszxqu4vIIkFw1B3UK8CH76W5Fu3pcFXhBpui0h/IvDHLePmddFfx/kptdZ0qCs0VxrZgltpjCD8PtWx5nde10xYLI6V/j6yFLao/flB0qt0SJxoIbUdI0Zk99TzOmR4A5yGdW158Nvcsd+bwXshHfmjn3uafksjdSnlcqyNClo1oR3pUJKX9dQuyLA6hlGzF5/f2Sf+eggOkpLcvY7/yStfQMFF6uLZq9DHQAlXsEVnFOmHBGqoFcRKNmV2I7kcJL9QMVxWJrNIkLjBmyxACCsf /home/vagrant/.ssh/id_rsa'
     ]
 }
