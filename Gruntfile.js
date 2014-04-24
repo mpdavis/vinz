1fdbcef1116250e6eb62eacd50bf1a0b8b14c60d
Jacob Hummel <j4cobhummel@gmail.com>
2014-04-24 17:56:31 -0500

    CSS Button Fix Dashboard Change

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,7 +31,7 @@ module.exports = function(grunt) {
             'bootstrap': bowerBase + 'bootstrap/',
             'jquery': bowerBase + 'jquery/',
             'fontAwesome': bowerBase + 'font-awesome/',
-            'chartjs': bowerBase + 'chartjs/'
+            'flot': bowerBase + 'flot/'
         },
         'build': {
             'js': buildBase + 'js/'
@@ -147,7 +147,7 @@ module.exports = function(grunt) {
                     directories.bower.angularRoute + 'angular-route.js',
                     directories.bower.angularCookies + 'angular-cookies.js',
                     directories.bower.angularSanitize + 'angular-sanitize.js',
-                    directories.bower.chartjs + 'Chart.min.js',
+                    directories.bower.flot + 'jquery.flot.js',
                     directories.frontend.js + 'app.js',
                     directories.frontend.js + 'controllers/logs.js',
                     directories.frontend.js + 'controllers/servers.js',
