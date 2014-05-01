c00701446c771f51e38a51ccc1dc407e5c683096
Michael Davis <mike.davis@webfilings.com>
2014-05-01 17:27:47 -0500

    JS linting

--- app/scripts/services/dashboard.js
+++ app/scripts/services/dashboard.js
@@ -2,21 +2,17 @@
 
 angular.module('vinzApp')
   .factory('dashboard', function ($http) {
-    // Service logic
-    // ...
+    
     var statsAPI = "/api/stats/";
     var logsAPI = "/api/scan_log_graph/";
 
-    //Users API
     return {
       getStats: function() {
-        var stats = $http.get(statsAPI);
-        return stats;
+        return $http.get(statsAPI);
       },
 
       getLogs: function() {
-        var logs = $http.get(logsAPI);
-        return logs
+        return $http.get(logsAPI);
       }
 
     };
