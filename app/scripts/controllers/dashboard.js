7580bfa9fb1f474c4c08e8e328667267302ab7c8
Michael Davis <mike.davis@webfilings.com>
2014-05-01 15:49:53 -0500

    flot settings

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -3,17 +3,17 @@
 angular.module('vinzApp')
   .controller('DashboardCtrl', ['$scope', 'dashboard', function ($scope, dashboard) {
 
-  	$scope.dashboardDimensions = {};
     $scope.flotOptions = {
-
+      'width': 500,
+      'height': 500
     };
   	dashboard.getStats().success(function(data, status, headers, config) {
     	$scope.stats = data;	
     });
 
     dashboard.getLogs().success(function(data, status, headers, config) {
-      $scope.graph_data = data;
+      $scope.graph_data = data.success;
     });
 
-    
+    $scope.graphData = [[1, 2], [2, 5], [3, 7]];
   }]);
